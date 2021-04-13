7b7a0abb96e23fda3b6a6f87946add4f566f406b
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-04-13 15:09:46 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,34 @@
+function SendProgram () {
+    for (let index = 0; index <= program.length - 1; index++) {
+        radio.sendString(program.substr(0, 10))
+    }
+    program = ""
+}
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("R")
+    basic.showLeds(`
+        . . . . .
+        # . # . .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    program = "" + program + "R"
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendString("L")
+    basic.showLeds(`
+        . . . . .
+        . . # . #
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    program = "" + program + "L"
 })
+input.onGesture(Gesture.Shake, function () {
+    program = ""
+})
+let program = ""
 radio.setGroup(1)
 basic.showString("Hello!")
 basic.showString("A for RIGHT!")
@@ -15,3 +40,5 @@ basic.showLeds(`
     . # # # .
     . . . . .
     `)
+program = "LRLR"
+SendProgram()
